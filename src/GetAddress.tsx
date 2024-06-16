5d4f2c87ede857844ee4a3fe5e99d69fb4e7e41a
ChaseBENNC9 <bennc9@student.op.ac.nz>
2024-06-16 21:03:40 +1200

    add: made returned address more precise

--- src/GetAddress.tsx
+++ src/GetAddress.tsx
@@ -7,11 +7,8 @@ export const GetAddress = async ({ lat, long }: position) => {
   const latlng = `${lat},${long}`;
   try {
     const res = await axios.get(
-      `${BASE_URL}?latlng=${latlng}&&key=${REACT_APP_GOOGLE_MAPS_API_KEY}`
+      `${BASE_URL}?latlng=${latlng}&location_type=ROOFTOP&result_type=street_address&key=${REACT_APP_GOOGLE_MAPS_API_KEY}`
     );
-    res.data.results.forEach((element: any) => {
-      console.log(element.formatted_address); 
-    });
     return res.data.results[0].formatted_address;
   } catch (error) {
     return "Error retrieving address";
