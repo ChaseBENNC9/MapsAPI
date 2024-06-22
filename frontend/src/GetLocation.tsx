3d178b248a1f15e2269494ffd7e4ec841a7fc3b4
ChaseBENNC9 <bennc9@student.op.ac.nz>
2024-06-22 22:57:37 +1200

    add: frontend now fetches from api

--- frontend/src/GetLocation.tsx
+++ frontend/src/GetLocation.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://192.168.68.65:3001";
+const BASE_URL = "https://mapsbackend.onrender.com";
 
 export const GetLocation = async () => {
 // Get Formatted address from lat and lng
