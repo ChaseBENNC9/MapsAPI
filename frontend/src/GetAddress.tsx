6fc9395e8e85bd71867580945460413b56f88b07
ChaseBENNC9 <bennc9@student.op.ac.nz>
2024-06-24 09:09:18 +1200

    remove: Rooftop filter for address type

--- frontend/src/GetAddress.tsx
+++ frontend/src/GetAddress.tsx
@@ -9,7 +9,7 @@ export const GetAddress = async ({ lat, lng }: position) => {
   const latlng = `${lat},${lng}`;
   try {
     const res = await axios.get(
-      `${BASE_URL}?latlng=${latlng}&location_type=ROOFTOP&result_type=street_address&key=${REACT_APP_GOOGLE_MAPS_API_KEY}`
+      `${BASE_URL}?latlng=${latlng}&result_type=street_address&key=${REACT_APP_GOOGLE_MAPS_API_KEY}`
     );
     console.log( `${BASE_URL}?latlng=${latlng}&location_type=ROOFTOP&result_type=street_address&key=${REACT_APP_GOOGLE_MAPS_API_KEY}`);
     return`${ res.data.results[0]["address_components"][0]["short_name"]} ${res.data.results[0]["address_components"][1]["short_name"]}`;
